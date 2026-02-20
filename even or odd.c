e90417cad4e2b691b78974844046f2589c3fa6de
indugolla8 <gollaindu.2005@gmail.com>
2026-02-20 21:42:11 +0530

    Add files via upload

---
+++ even or odd.c
@@ -0,0 +1,15 @@
+//Check even or odd
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("It is even");
+    }
+    else
+    {
+        printf("It is not even");
+    }
+}
